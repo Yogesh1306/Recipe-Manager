a296a85c0db09ad775127034e5acf6e58c5d9afc
Yogesh Joshi <work.yogeshjoshi@gmail.com>
2025-11-21 19:00:01 +0530

    Implement API error and response handling classes; add async handler utility

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,18 @@
 import dotenv from "dotenv";
 import { connectDB } from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
 connectDB()
-  .then(() => {})
-  .catch(() => {});
+  .then(() => {
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Database connection failed!!!\nError: ${err}`);
+  });
