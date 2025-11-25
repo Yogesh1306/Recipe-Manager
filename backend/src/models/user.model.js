ae327d9540792fd90f6734be8c2d86d2178e11f0
Yogesh Joshi <work.yogeshjoshi@gmail.com>
2025-11-25 13:50:56 +0530

    Implement JWT authentication middleware and enhance user model with password hashing and token generation

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -28,7 +29,7 @@ const userSchema = new Schema(
       {
         type: Schema.Types.ObjectId,
         ref: "Recipe",
-      }
+      },
     ],
     refreshToken: {
       type: String,
@@ -36,5 +37,40 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+// Pre-save hook to hash password before saving user document
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  // Hash the password with a salt round of 10
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
 
 export const User = new mongoose.model("User", userSchema);
