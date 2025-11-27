62b3b5456df1453f42f92577cc177435d8dbe495
Yogesh Joshi <work.yogeshjoshi@gmail.com>
2025-11-27 18:34:21 +0530

    Implement createRecipe controller with image upload to Cloudinary

---
+++ backend/src/controllers/recipe.controllers.js
@@ -0,0 +1,44 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
+import { Recipe } from "../models/recipe.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+
+const createRecipe = asyncHandler(async (req, res) => {
+  const { title, description, ingredients, steps, tags } = req.body;
+
+  const imgLocalPath = req.file?.path;
+  if (!imgLocalPath) {
+    throw new ApiError(400, "Recipe img is required!!");
+  }
+
+  const img = await uploadOnCloudinary(imgLocalPath);
+  if (!img) {
+    throw new ApiError(400, "Error while uploading Recipe img to cloudinary");
+  }
+
+  const recipe = await Recipe.create({
+    title,
+    description,
+    ingredients,
+    steps,
+    tags,
+    img: img.url,
+    user: req.user,
+  });
+
+  const createdRecipe = await Recipe.findOne(recipe?._id);
+  if (!createdRecipe) {
+    throw new ApiError(
+      500,
+      "Something went wrong while creating the recipe...Try again!!"
+    );
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, createdRecipe, "Created successfully!!"));
+});
+
+export { createRecipe };
