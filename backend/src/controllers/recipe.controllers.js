39af1cc727f20e2e5bb17ba7598b1245a974b57a
Yogesh Joshi <work.yogeshjoshi@gmail.com>
2025-11-27 19:41:40 +0530

    Implement updateRecipeImg controller for updating recipe images

--- backend/src/controllers/recipe.controllers.js
+++ backend/src/controllers/recipe.controllers.js
@@ -41,4 +41,34 @@ const createRecipe = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdRecipe, "Created successfully!!"));
 });
 
-export { createRecipe };
+const updateRecipeImg = asyncHandler(async (req, res) => {
+  const recipeId = req.params.recipeId;
+
+  const recipe = await Recipe.findById({ _id:recipeId, user: req.user._id });
+  if (!recipe) {
+    throw new ApiError(404, "No data available");
+  }
+
+  const imgLocalPath = req.file?.path;
+  if (!imgLocalPath) {
+    throw new ApiError(400, "Recipe img is required!!");
+  }
+  const img = await uploadOnCloudinary(imgLocalPath);
+  if (!img) {
+    throw new ApiError(400, "Error while uploading Recipe img to cloudinary");
+  }
+  const updatedImg = await Recipe.findByIdAndUpdate(
+    recipe._id,
+    {
+      $set: {
+        img: img.url,
+      },
+    },
+    { new: true }
+  );
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedImg, "Img updated successfully"));
+});
+
+export { createRecipe, updateRecipeImg };
