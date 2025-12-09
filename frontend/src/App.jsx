076346c36b7113a275043c56e1892c4de611f363
Yogesh Joshi <work.yogeshjoshi@gmail.com>
2025-12-09 18:34:52 +0530

    feat: add routing for login and signup pages, and include PageNotFound component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import PageNotFound from "./pages/PageNotFound";
+
 
 const router = createBrowserRouter([
   {
     path:"/",
     element:<><Home/></>
+  },
+  {
+    path:"/login",
+    element:<><Login/></>
+  },
+  {
+    path:"/signup",
+    element:<><Signup/></>
+  },
+  {
+    path: "*",
+    element:<PageNotFound/>
   }
 ]);
 const App = () => {
